d17f48fa63f252f0bd9cc3119077bd933d59893a
Jupinder14 <jupindersingh08@gmail.com>
2023-03-19 17:49:40 -0400

    add wedriver

--- features/environment.py
+++ features/environment.py
@@ -1,7 +1,9 @@
 from selenium import webdriver
+from selenium.webdriver.chrome.service import Service as ChromeService
+from webdriver_manager.chrome import ChromeDriverManager
 from utils.pages.calorie_calc import CarbCalcPage
 
 def before_all(context):
-    context.driver = webdriver.Chrome()
+    context.driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
     context.driver.maximize_window()
     context.calculator = CarbCalcPage()
