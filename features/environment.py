a701913d7a87693322ad358363ba5bcb7d3ce71e
Jupinder14 <jupindersingh08@gmail.com>
2023-03-19 23:31:19 -0400

    run chrome in headless

--- features/environment.py
+++ features/environment.py
@@ -1,9 +1,12 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service as ChromeService
 from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.options import Options
 from utils.pages.calorie_calc import CarbCalcPage
 
 def before_all(context):
-    context.driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
+    chrome_options = Options()
+    chrome_options.add_argument("--headless")
+    context.driver = webdriver.Chrome(options=chrome_options, service=ChromeService(ChromeDriverManager().install()))
     context.driver.maximize_window()
     context.calculator = CarbCalcPage()
